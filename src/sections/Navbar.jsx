4b9d81b810989ab2e984c19a2b880312e3e6f0e8
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2025-03-12 21:46:10 +0000

    create and style Navbar desktop component

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
+import { navLinks } from "../constants";
 
 const NavItems = () => {
     return (
         <ul className="nav-ul">
-            {}
+            {navLinks.map(({ id, href, name }) => (
+                <li key={id} className="nav-li">
+                    <a href={href} className="nav-li_a" onClick={() => {}}>
+                        {name}
+                    </a>
+                </li>
+            ))}
         </ul>
     )
 }
