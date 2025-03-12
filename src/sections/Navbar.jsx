be56a0192136adf27068429702976cd5281f9814
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2025-03-12 21:40:19 +0000

    add constants file

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+
+const NavItems = () => {
+    return (
+        <ul className="nav-ul">
+            {}
+        </ul>
+    )
+}
+
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => setIsOpen((prevIsOpen) => !prevIsOpen);    
+
     return (
         <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
             <div className="max-w-7xl mx-auto">
@@ -7,13 +21,21 @@ const Navbar = () => {
                         João Gonçalves
                     </a>
 
-                    <button>
+                    <button 
+                        onClick={toggleMenu} 
+                        className="text-neutral-400 hover:text-white focus:outline-hidden sm:hidden flex" 
+                        aria-label="Toggle Menu"
+                    >
                         <img 
-                            src={"assets/menu.svg"} 
+                            src={isOpen ? "assets/close.svg" : "assets/menu.svg"} 
                             alt="Toggle" 
                             className="w-6 h-6" 
                         />
                     </button>
+
+                    <nav className="sm:flex hidden">
+                        <NavItems />
+                    </nav>
                 </div>
             </div>
         </header>
