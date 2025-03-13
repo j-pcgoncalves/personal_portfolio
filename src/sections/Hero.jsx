ffcba90b9557af952e7851134c7534dcf902e7d0
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2025-03-13 23:20:45 +0000

    add light, make model responsive and position correctly

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,41 +1,52 @@
 import { PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
+import { useMediaQuery } from "react-responsive";
 
 import HackerRoom from "../components/HackerRoom";
 import CanvasLoader from "../components/CanvasLoader";
+import { calculateSizes } from "../constants";
 
 const Hero = () => {
-  return (
-    <section className="min-h-screen w-full flex flex-col relative">
-        <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-            <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
-                Hi, I'm João 
-                <span className="waving-hand">👋</span>
-            </p>
-            <p className="hero_tag text-gray_gradient">
-                Building smooth and reliable web apps
-            </p>
-        </div>
+    const isSmall = useMediaQuery({ maxWidth: 440 });
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
 
-        <div className="w-full h-full absolute inset-0">
-            <Canvas className="w-full h-full">
-                <Suspense fallback={<CanvasLoader />}>
-                    <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+    const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
-                    <HackerRoom 
-                        scale={0.07} 
-                        position={[0, 0, 0]} 
-                        rotation={[0, 280, 0]} 
-                    />
-                    
-                    <ambientLight intensity={1} />
-                    <directionalLight position={[10, 10, 10]} intensity={0.5} />
-                </Suspense>
-            </Canvas>
-        </div>
-    </section>
-  );
+    return (
+        <section className="min-h-screen w-full flex flex-col relative">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+                    Hi, I'm João 
+                    <span className="waving-hand">👋</span>
+                </p>
+                <p className="hero_tag text-gray_gradient">
+                    Building reliable web apps
+                </p>
+            </div>
+
+            <div className="w-full h-full absolute inset-0">
+                {/* <Leva /> */}
+
+                <Canvas className="w-full h-full">
+                    <Suspense fallback={<CanvasLoader />}>
+                        <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+
+                        <HackerRoom 
+                            scale={sizes.deskScale} 
+                            position={sizes.deskPosition} 
+                            rotation={[0, -Math.PI, 0]} 
+                        />
+
+
+                        <ambientLight intensity={1} />
+                        <directionalLight position={[10, 10, 10]} intensity={0.5} />
+                    </Suspense>
+                </Canvas>
+            </div>
+        </section>
+    );
 };
 
 export default Hero;
